669beddf804eca9fdf4b2fca15330103fb7b2f06
prabodh44 <prabodhtuladhar44@gmail.com>
2018-10-09 09:30:02 +0545

    Modified the data addition and data display logic

--- js/script.js
+++ js/script.js
@@ -8,12 +8,20 @@ var config = {
     messagingSenderId: "517562225371"
 };
 
+//initialize firebase
 firebase.initializeApp(config);
+
+//database instance
 var database = firebase.database();
 
 
 var Voucher = angular.module('Voucher', ['ngMaterial', 'ngMessages']);
 Voucher.controller('MainController', function ($scope, $http) {
+    
+    //root database reference
+
+    var rootRef = database.ref();
+    
     var self = this;
     var namesArray = [];
 
@@ -24,32 +32,17 @@ Voucher.controller('MainController', function ($scope, $http) {
     self.searchText = null;
     self.querySearch = querySearch;
 
-    // ******************************
-    // Internal methods
-    // ******************************
-
-    /**
-     * Search for states... use $timeout to simulate
-     * remote dataservice call.
-     */
     function querySearch(query) {
         var results = query ? namesArray.filter(createFilterFor(query)) : namesArray;
         return results;
     }
 
-
-
-    /**
-     * Build `states` list of key/value pairs
-     */
     function loadAll() {
-        $http.get('data/data.json')
-            .then(function (data) {
-                console.log(data);
-                namesArray = data.data;
-            }, function (error) {
-                console.log(error);
-            });
+        var userRef = rootRef.child('users');
+        userRef.on('child_added', function(snapshot){
+                namesArray.push(snapshot.val()["username"]);
+            console.log(namesArray);
+        });
     }
 
     /**
@@ -58,8 +51,8 @@ Voucher.controller('MainController', function ($scope, $http) {
     function createFilterFor(query) {
         //var lowercaseQuery = query.toLowerCase();
 
-        return function filterFn(state) {
-            return (state.name.indexOf(query) === 0);
+        return function filterFn(data) {
+            return (data.indexOf(query) === 0);
         };
 
     }
@@ -67,46 +60,43 @@ Voucher.controller('MainController', function ($scope, $http) {
    
 
     $scope.addUser = function (username) {
-
-        var ref = database.ref();
-        var userRef = ref.child('users');
+        var userRef = rootRef.child('users');
 
         var obj = {
             username: username,
             date: new Date().toDateString(),
         };
 
-       // userRef.push().set(obj);
+       userRef.push().set(obj);
     }
 
-    $scope.saveData = function (selectedItem, amount) {
-
-        var rootRef = database.ref();
-        var userRef = rootRef.child('users');
-   
-        // firebase.database().ref('users/' + "1234").set({
-        //     username: selectedItem.name,
-        //         amount: amount,
-        //         date: new Date().toDateString()
-        //   });
+    $scope.saveData = function (username, amount) {
+        var userRef = rootRef.child('transactions');
         var obj = {
-            username: selectedItem.name,
+            username: username,
             amount: amount,
             date: new Date().toDateString()
         };
         userRef.push().set(obj);
-    }
+    }   
 
 
     $scope.readData = function () {
         var rootRef = database.ref();
-        var userRef = rootRef.child('users');
-        userRef.once('value')
-        .then(function(snapshot){
-            for(var key in snapshot.val()){
-                console.log(snapshot.val()[key]);
-            }
-        });
+        var transRef = rootRef.child('transactions');
+    //     userRef.once('value')
+    //     .then(function(snapshot){
+    //         for(var key in snapshot.val()){
+    //             $scope.results.push(snapshot.val()[key]);
+    //         }
+    //     });
+
+    rootRef.orderByChild('transactions').equalTo('amount').once('value', function(snapshot){
+        console.log(snapshot);
+        var a = "a";
+    });
     }
+
+    
 
 });
